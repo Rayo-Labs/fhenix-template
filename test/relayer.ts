8099556363b017206efc5a16d95b921c5ec3b90c
grkmyvz <gorkem.yavuz.39@hotmail.com>
2024-09-13 00:29:22 +0300

    sadfsd

--- test/relayer.ts
+++ test/relayer.ts
@@ -2,65 +2,49 @@ import { FhenixClient, getPermit } from "fhenixjs";
 import hre from "hardhat";
 import deployments from "../deployments/testnet/FhenixBridge.json";
 
-const contractAddress = deployments.address;
+import { abi } from "./zama/abi";
 
-const contractABI = deployments.abi
+// # FOR FHENIX
+// const contractAddress = deployments.address;
+// const contractABI = deployments.abi;
+
+// # FOR ZAMA
+const contractAddress = "0xD581086aE74B6e5936201A30D264E17D14eAfF0C";
+const contractABI = abi;
 
 const { fhenixjs, ethers } = hre;
 
 const fhenixClient = new FhenixClient({ provider: ethers.provider });
 
-const contract = new ethers.Contract(contractAddress, contractABI, ethers.provider);
+const provider = new ethers.JsonRpcProvider("https://devnet.zama.ai");
+
+const contract = new ethers.Contract(
+  contractAddress,
+  contractABI,
+  provider, // ethers.provider
+);
 
 let permit;
 
 async function main() {
-  permit = await getPermit(contractAddress, ethers.provider);
-  fhenixClient.storePermit(permit);
-  console.log("Permit", permit);
+  // permit = await getPermit(contractAddress, ethers.provider);
+  // if (!permit) {
+  //   throw new Error("Permit not found");
+  // }
+  // fhenixClient.storePermit(permit);
+  // console.log("Permit", permit);
 
-  contract.on('Packet', (log1, log2, log3, log4, log5) => {
-    console.log("Packet Events", log1, log2, log3, log4, log5);
+  console.log("Watching for events...");
 
-    const clearTo = fhenixClient.unseal(contractAddress, log3);
-    const clearAmount = fhenixClient.unseal(contractAddress, log4)
+  contract.on("TestPacket", (log) => {
+    console.log("Packet Events", log);
 
-    console.log("Clear To", clearTo.toString(16));
-    console.log("Clear Amount", clearAmount);
-    /*     const parsedEvent = JSON.parse(logs);
-        console.log("Parsed Event", parsedEvent);
-        const cleartext = fhenixClient.unseal(contractAddress, logs)
-        console.log("Cleartext", cleartext); */
+    // const clearTo = fhenixClient.unseal(contractAddress, log3);
+    // const clearAmount = fhenixClient.unseal(contractAddress, log4)
 
-    /*     if (events.length === 0) return;
-    
-        events.forEach((event) => {
-          console.log("Packet", event);
-          try {
-            const decodedEvent = decodeEventLog({
-              abi: fhenixBridgeABI,
-              data: event.data,
-              topics: event.topics,
-            });
-            //const decodedPacket = decodedEvent.args as DecodedPacket;
-            console.log("Decoded Event", decodedEvent);
-            const jsonString = JSON.stringify(
-              decodedEvent,
-              (key, value) => {
-                return typeof value === "bigint" ? value.toString() : value;
-              },
-              2
-            );
-            if (!jsonString) {
-              console.error("No JSON String");
-              return;
-            }
-            fs.writeFileSync("decodedEvent.json", jsonString);
-          } catch (error) {
-            console.error("Decode Error: ", error);
-          }
-        }); */
+    // console.log("Clear To", clearTo.toString(16));
+    // console.log("Clear Amount", clearAmount);
   });
 }
 
-main()
+main();
---
+++ test/zama/abi.ts
@@ -0,0 +1,314 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_tokenAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "OnlyRelayer",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "encryptedAmount",
+        type: "uint256",
+      },
+    ],
+    name: "IntentProcessed",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferStarted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "eaddress",
+        name: "to",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "euint64",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "relayer",
+        type: "address",
+      },
+    ],
+    name: "Packet",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "num",
+        type: "uint256",
+      },
+    ],
+    name: "TestPacket",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "acceptOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "einput",
+        name: "_encryptedTo",
+        type: "bytes32",
+      },
+      {
+        internalType: "einput",
+        name: "_encryptedAmount",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "_inputProof",
+        type: "bytes",
+      },
+      {
+        internalType: "address",
+        name: "_relayerAddress",
+        type: "address",
+      },
+    ],
+    name: "bridgeWEERC20",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_to",
+        type: "address",
+      },
+      {
+        internalType: "einput",
+        name: "_encryptedAmount",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "inputProof",
+        type: "bytes",
+      },
+    ],
+    name: "onRecvIntent",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pendingOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "relayers",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_relayer",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "_status",
+        type: "bool",
+      },
+    ],
+    name: "setRelayer",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "testEmit",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "weerc20",
+    outputs: [
+      {
+        internalType: "contract IZamaWEERC20",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "einput",
+        name: "_encryptedAmount",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "_inputProof",
+        type: "bytes",
+      },
+    ],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
